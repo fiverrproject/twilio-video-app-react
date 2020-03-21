ead2e282bdbaed7eada1641fb9df894d72a14e33
Stepupithub <stepupithub@gmail.com>
2020-03-21 17:53:54 +0530

    adde trello code

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+require('dotenv').config();
+
+const MAX_ALLOWED_SESSION_DURATION = 14400;
+const twilioAccountSid = 'AC015ceea6e72b7ae090e8291eb30a6935';
+const twilioApiKeySID = 'SKbf7a181dd0cf3639fe1059e99eb2ee2a';
+const twilioApiKeySecret = 'GqgW0qU3T7ea3xgbr0kErXjagyVD7bIv';
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/token', (req, res) => {
+  const { identity, roomName } = req.query;
+  const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
+    ttl: MAX_ALLOWED_SESSION_DURATION,
+  });
+  token.identity = identity;
+  const videoGrant = new VideoGrant({ room: roomName });
+  token.addGrant(videoGrant);
+  res.send(token.toJwt());
+  console.log(`issued token for ${identity} in room ${roomName}`);
+});
+
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
+
+app.listen(8081, () => console.log('token server running on 8081'));
